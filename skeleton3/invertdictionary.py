64a59ec45d53195c890683e2db504558df6daeca
Schmalhans <lqfb@glottotopia.de>
2016-09-20 12:26:53 +0200

    with sensenumbers in inverted index entries, cf. #12

--- skeleton3/invertdictionary.py
+++ skeleton3/invertdictionary.py
@@ -4,26 +4,27 @@ import re
 
 class Entry():
   def __init__(self,a):
+    sensenr = ''
+    self.inverts = []
+    poss = ''
     for l in a:
       l = l.strip()
       if l.endswith('%'):
-	l = l[:-1]      
-      if 'lsgloss' in l:
-	self.gloss = l[9:][:-1]
-      #elif 'definition' in l:
-	#self.definition = l      
+        l = l[:-1]          
       if r'\headword' in l:
-	try:
-	  self.vernaculars.append(l)
-	except AttributeError:
-	  self.vernaculars = [l]
-	continue            
+        headword=l          
+        continue
+      if r'\sensenr' in l:
+        sensenr = l
+        continue
       if l.strip().startswith(r'\synpos') or l.strip().startswith(r'\pos'):
-	try:
-	  self.poss.append(l)
-	except AttributeError:
-	  self.poss = [l]
-      	continue
+        poss = l
+        continue
+      if 'lsgloss' in l:         
+        gloss = l[9:][:-1]          
+        self.inverts.append((gloss, headword, sensenr, poss))
+ 
+
        
 d = {}
 
@@ -34,15 +35,16 @@ for e in entries[1:]:
   a = e.split('\n')[1:]
   x = Entry(a)
   try:
-    for g in x.gloss.split(';'):
-    #print a
-      try:
-	d[g.strip()].append(x)
-      except KeyError:
-	d[g.strip()] = [x]
-      except AttributeError:     
-	s =  u"%% no gloss for %s" % x.vernaculars
-	print s.encode('utf8')
+    for inv in x.inverts:
+      glosses = inv[0]
+      for g in glosses.split(';'): 
+        try:
+          d[g.strip()].append(inv[1:])
+        except KeyError:
+          d[g.strip()] = [inv[1:]]
+        except AttributeError:     
+          s =  u"%% no gloss for %s" % x.vernaculars
+          print s.encode('utf8')
   except AttributeError:
     continue
       
@@ -52,10 +54,9 @@ for k in sorted(d.keys(),key=lambda s: s.lower()):
   print u'\\lsgloss{%s}' % k.strip().encode('utf8')
   out = []
   for e in d[k]:
-    try:
-      out.append(''.join(['~'.join(l)
-      for l in zip(e.vernaculars,e.poss)]))      
-    except AttributeError:
-      print "%%insufficient information for %s".encode('utf8') % k
+    out.append("""%s%%
+%s%%
+%s%%
+"""%e)
   print ';\n'.join(out).encode('utf8')      
   
